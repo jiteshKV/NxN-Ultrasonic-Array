87030c9dd29c5b1c1137be03238287dba68c2f06
jiteshKV <34055442+jiteshKV@users.noreply.github.com>
2026-04-04 23:56:22 -0400

    Add files via upload

---
+++ functions/getDateString.m
@@ -0,0 +1,34 @@
+function date_string = getDateString()
+%GETDATESTRING Create a string of the current date and time.
+%
+% DESCRIPTION:
+%     getDateString returns a string of the current date and time using
+%     datetime in the following format: 'dd-MMM-yyyy-HH-mm-ss'.
+%
+% USAGE:
+%     date_string = getDateString()
+%
+% ABOUT:
+%     author      - Bradley Treeby
+%     date        - 14th October 2009
+%     last update - 4th June 2017
+%       
+% This function is part of the k-Wave Toolbox (http://www.k-wave.org)
+% Copyright (C) 2009- Bradley Treeby
+%
+% See also: datetime
+% This file is part of k-Wave. k-Wave is free software: you can
+% redistribute it and/or modify it under the terms of the GNU Lesser
+% General Public License as published by the Free Software Foundation,
+% either version 3 of the License, or (at your option) any later version.
+% 
+% k-Wave is distributed in the hope that it will be useful, but WITHOUT ANY
+% WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+% FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+% more details. 
+% 
+% You should have received a copy of the GNU Lesser General Public License
+% along with k-Wave. If not, see <http://www.gnu.org/licenses/>. 
+
+% get the current time
+date_string = char(datetime("now", "Format", "dd-MMM-yyyy-HH-mm-ss"));
